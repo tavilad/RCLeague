24889ffbc224ca66e22e6977131e7bd17c326f8f
tavilad <pintiliciucoctavian@gmail.com>
2017-10-08 23:29:27 +0300

    test

---
+++ Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+    public float moveSpeed = 10f;
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+
+        //print(Input.GetAxis("Horizontal"));
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical")*Time.deltaTime);
+
+	}
+
+
+
+}
